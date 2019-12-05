a47e3be85aa1c9cb6e9ce8d14542a3208c77ef18
xiongwei.zhu <xiongwei.zhu@yingzt.com>
2019-12-05 14:17:02 +0800

    fix: go module error for etcd/clientv3

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/coreos/etcd v3.3.18+incompatible
+	github.com/coreos/go-systemd v0.0.0-00010101000000-000000000000 // indirect
 	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
 	github.com/go-redis/redis/v7 v7.0.0-beta.4
 	github.com/gogo/protobuf v1.3.1 // indirect
@@ -24,3 +25,5 @@ require (
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v2 v2.2.7 // indirect
 )
+
+replace github.com/coreos/go-systemd => github.com/coreos/go-systemd/v22 v22.0.0
---
+++ rpc/gRPC/registerClient.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"github.com/xiongwei9/Gogogo/rpc/gRPC/constant"
+	pb "github.com/xiongwei9/Gogogo/rpc/gRPC/proto"
+	"github.com/xiongwei9/Gogogo/rpc/gRPC/registerCenter"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"log"
+)
+
+func main() {
+	schema, err := registerCenter.GenerateAndRegisterEtcdResolver(constant.EtcdAddress, "HelloService")
+	if err != nil {
+		log.Fatalf("init etcd resolver error: %v", err)
+	}
+
+	address := fmt.Sprintf("%s:///HelloService", schema)
+	conn, err := grpc.Dial(address, grpc.WithInsecure()) // 无SSL认证
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer func() {
+		err := conn.Close()
+		if err != nil {
+			log.Fatalf("close connection error: %v", err)
+		}
+	}()
+
+	c := pb.NewGreeterClient(conn)
+	message := "hello, gRPC"
+
+	r, err := c.SayHello(context.Background(), &pb.HelloRequest{Message: message})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+}
